c55727455e6ff2d12fb127d3140e446dc96d6d81
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-03-11 18:30:08 -0300

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+const cors = require('cors')
 
 const app = express()
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Access-Control-Allow-Origin']
+}))
 
 const port = process.env.PORT || 5000
 
